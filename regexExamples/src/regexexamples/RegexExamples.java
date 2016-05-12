a09d44116a20207ccbc634e87147b5e5bc851ab5
Alan Gonzalez <marioalangonzalez@gmail.com>
2016-05-12 19:50:44 -0300

    example

--- regexExamples/src/regexexamples/RegexExamples.java
+++ regexExamples/src/regexexamples/RegexExamples.java
@@ -23,27 +23,22 @@ public class RegexExamples {
          Pattern bold = new Pattern("\\*(\\p{Graph}+\\s*)\\*"); //bold
          Pattern italic = new Pattern("#(\\p{Graph}+\\s*)#"); //italic
          
+         //Texto a parsear que tiene palabras en negrita, cursiva y combinadas de las dos formas
          String texto = "esto es texto con una palabra en *negrita* otra en #cursiva# y otra en #*negritacursiva*# y *#otra#*";
          String result = "";
          
+         //Primero reemplazo las negritas
          Replacer r = bold.replacer("<strong>$1</strong>");
          texto = r.replace(texto);
-         
+         //imprimo lo que resulta solo de reemplazar negritas
          System.out.println(texto);
          
+         //Tomo el texto con las negritas ya reemplazadas y reemplazo las cursivas
          Replacer r2 = italic.replacer("<em>$1</em>");
          result = r2.replace(texto);
          
+         //Muestro resultado final
          System.out.println(result);
-         
-         
-         
-         /*Matcher m=p.matcher(texto);
-         while(m.find()){
-            System.out.println("next word: ["+m.toString()+"]");
-        }*/
-         
-       
     }
     
 }
